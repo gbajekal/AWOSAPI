299f42ddd49a1fc8cce37c477599e70eaef37f31
bajga01 <bajga01@BAJGA01-M4800.ca.com>
2016-03-30 15:45:59 +0530

    Initial checkin

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  use google\appengine\api\users\User;
  use google\appengine\api\users\UserService;
  use \google\appengine\api\mail\Message;
-$DEBUG = TRUE;
+$DEBUG = FALSE;
 
 class AWOS_API extends REST {
 
@@ -12,6 +12,7 @@ class AWOS_API extends REST {
 
 
     const DB_SERVER = "/cloudsql/awos-beta:awos";
+   //const DB_SERVER = "localhost";
     const DB_USER = "root";
     const DB_PASSWORD = "admin";
     const DB = "awos";
@@ -36,14 +37,24 @@ class AWOS_API extends REST {
     
     function processApi()
     {
+        
+       
+        
+        
         $this->log("Entered processApi()");
+        
+        // Get email of logged in user
+        $user = $this->validateTokenAndGetEmail();
+        
+        
+        
         # Looks for current Google account session
-        $user = UserService::getCurrentUser();
+      //  $user = UserService::getCurrentUser();
         
       if( $user)
       { 
           
-          
+         $this->log("User is: ".$user);
           
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $func = strtolower(trim(str_replace("/", "", $path)));
@@ -102,7 +113,7 @@ class AWOS_API extends REST {
     function complaints($user)
     {
         $this->log("Entered complaints()");
-        if ($this->get_request_method() == "GET") {
+        if ($this->get_request_method() == "POST") {
            //***************************
             // Check if the logged in user
             // is an Admin. In which case
@@ -113,15 +124,18 @@ class AWOS_API extends REST {
         
         if($user) 
         {
-            if( $isAdmin)
+           /* if( $isAdmin)
             {
+                $this->log("Logged User is  admin");
                 $historyQuery = 'Select a.*, b.block, b.apt, b.Mobile from grievance a, users b 
                           where a.Submitter=b.userid';
             }
             else {
-                  $historyQuery = "Select * from grievance where Submitter='$email'";
+            */      
+                $this->log("User is not admin");
+                $historyQuery = "Select * from grievance where Submitter='$user'";
 
-                }
+                
               // $sql = mysql_query($historyQuery, $this->db);
                 
                 $queryResult = $this->db->query($historyQuery);
@@ -135,18 +149,106 @@ class AWOS_API extends REST {
                     
                 
                    
-        }
+        
                 $this->deliver_response("200", "Complaints", $result);
              
         }   
         else {
               $this->deliver_response("403", "Forbidden", null);
         }
-        $this->log("Exited complaints()");
+        
+    }
+ else {
+            $this->log("Request is not POST");
     }
     
+    $this->log("Exited complaints()");
+    }
     
+    //***********************************
+    // This method returns the list of 
+    // categories
+    function categories($user)
+    {
+        $this->log("Entered categories");
+        $Catquery = "Select * from categories";
+        $queryResult = $this->db->query($Catquery);
+                
+         $result = array();
+         
+         while($category = $queryResult->fetch_assoc())
+         {
+             $result[] = $category;
+         }
+        
+         $this->deliver_response("200", "Categories", $result);
+        
+        $this->log("Exited categories");
+    }
     
+    //**************************************************
+    // addComplaint() - Adds/updates a complaint to the database
+    //***************************************************
+    function add($user)
+    {
+        $this->log('Entered addComplaint()');
+
+        if ($this->get_request_method() == "POST") {
+            $complaint = htmlspecialchars($_POST['complaint']);
+            $this->log('complaint=' . $complaint);
+            $compCategory = htmlspecialchars($_POST['category']);
+            $this->log('category=' . $compCategory);
+            $createdDate = date("d-m-Y h:i:s", time());
+            $complaintResult;
+            $email = $user;
+            $status = htmlspecialchars($_POST['status']);
+            $id = htmlspecialchars($_POST['id']);
+            
+            try {
+            
+            //********************************
+            // Check if action is add or update
+            //***********************************
+                $dbCmd = null;
+                
+                if ($id == null) {
+                    //*******************************
+                    // Action is to Add a new entry
+                    //********************************
+                 
+                    $dbCmd = "INSERT INTO `grievance`(`Complaint`, `Date_Created`, `Status`,  `Category`, `Submitter`)
+         VALUES ('$complaint',now(),'$status','$compCategory', '$email')";
+                } else {
+
+                    //*************************************
+                    // Action is update an existing entry
+                    // Save status and resolved date
+                    //*************************************
+                   $resolvedDate = date("Y-m-d");
+                   
+                    $dbCmd = "UPDATE grievance SET Status = '$status', Date_Resolved = '$resolvedDate' where id = '$id'";
+                }
+
+                $complaintResult = $this->db->query($dbCmd);
+            } catch (Exception $ex) {
+
+                echo "Error in inserting = " . $ex->getMessage();
+            }
+            $this->log("email=" . $email);
+            $this->log("Complaint Result=" . $complaintResult);
+
+            if ($complaintResult == TRUE) {
+                $this->deliver_response("200", "savedStatus", "OK");
+            } else {
+                $this->deliver_response("300", "savedStatus", "Error");
+            }
+        } else {
+            $this->log('Request method is GET instead of POST');
+        }
+
+        $this->log('Exited addComplaint()');
+    }
+
     public function deliver_response($status, $statusMsg, $data)
 {
     header("HTTP/1/1 $status $statusMsg");
@@ -165,6 +267,12 @@ class AWOS_API extends REST {
                  echo $logString .'<br>';               
                     
                 }
+                
+                
+  private function validateTokenAndGetEmail()
+  {
+     return $_REQUEST['user'];
+  }
     
 }; // end class
 
